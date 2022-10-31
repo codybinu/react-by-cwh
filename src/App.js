3cdca22db49cbabfd10afccdf5bb2a6e63c3a50b
codybinu <binayak.bidyasagar99@gmail.com>
2022-10-31 22:56:01 +0530

    using state to change the CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       {/* <Navbar title="TextUtils" aboutText="About TextUtils"/> */}
       <Navbar title="TextUtils" />
       <div className="container my-3">
-        <TextForm heading="Enter the text to analyze below" />
+        {/* <TextForm heading="Enter the text to analyze below" /> */}
+        <About/>
       </div>
     </>
 
