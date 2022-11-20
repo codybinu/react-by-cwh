905e37aa2f9e090ff4562eb107d35fb8fa73fafe
codybinu <binayak.bidyasagar99@gmail.com>
2022-11-20 14:39:36 +0530

    code updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
   })
   const [btnText, setBtnText] = useState('Enable Dark Mode')
 
-  //toggling the background color
+  //toggling the background color afterr clicking the button
   const toggleStyle = () => {
     if (myStyle.color === "black") {
       setMyStyle({
